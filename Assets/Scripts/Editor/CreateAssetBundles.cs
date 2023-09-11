3818c4330385671b02aa709554c16b53512ce78c
quannguyen7901 <quannguyen1879@gmail.com>
2023-09-11 16:13:35 +0700

    fixGit

--- Assets/Scripts/Editor/CreateAssetBundles.cs
+++ Assets/Scripts/Editor/CreateAssetBundles.cs
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using UnityEditor;
-using UnityEngine;
-
-public class CreateAssetBundles
-{
-    [MenuItem("Assets/Build AssetBundles")]
-    static void BuildAllAssetBundles()
-    {
-        string assetBundleDirectory = "Assets/AssetBundles";
-        if (!Directory.Exists(assetBundleDirectory))
-        {
-            Directory.CreateDirectory(assetBundleDirectory);
-        }
-        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
-=======
-=======
->>>>>>> origin/main
-=======
->>>>>>> origin/main
 using System;
 using UnityEngine;
 using UnityEditor;
@@ -50,12 +25,5 @@ public class CreateAssetBundles
         { 
             Debug.LogException(e);
         }
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 7096988c3f59170432ecc0d0437a7ad994ddc8d0
-=======
->>>>>>> origin/main
-=======
->>>>>>> origin/main
     }
 }
--- Assets/Scripts/LoadAssetBundles.cs
+++ Assets/Scripts/LoadAssetBundles.cs
@@ -1,58 +1,25 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 using System;
-=======
->>>>>>> 7096988c3f59170432ecc0d0437a7ad994ddc8d0
-=======
->>>>>>> origin/main
-=======
->>>>>>> origin/main
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 using UnityEngine.Assertions;
 
 public class LoadAssetBundles : MonoBehaviour
 {
     public Transform Parent;
-    void Start()
+    private void Start()
     {
         var myLoadedAssetBundle
             = AssetBundle.LoadFromFile("Assets/AssetBundles/uis").LoadAllAssets();
-=======
-=======
->>>>>>> origin/main
-=======
->>>>>>> origin/main
-
-public class LoadAssetBundles : MonoBehaviour
-{
-    private void Start()
-    {
-        var myLoadedAssetBundle = AssetBundle.LoadFromFile("Assets/AssetBundles/pagesassetbundles");
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 7096988c3f59170432ecc0d0437a7ad994ddc8d0
-=======
->>>>>>> origin/main
-=======
->>>>>>> origin/main
         if (myLoadedAssetBundle == null)
         {
             Debug.Log("Failed to load AssetBundle!");
             return;
         }
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-        foreach(var assetBundle in myLoadedAssetBundle)
+        foreach (var assetBundle in myLoadedAssetBundle)
         {
-            var type=assetBundle.GetType();
+            var type = assetBundle.GetType();
             if (type.ToString().Contains("GameObject"))
             {
                 Debug.Log(assetBundle.GetType());
@@ -64,17 +31,5 @@ public class LoadAssetBundles : MonoBehaviour
                 Instantiate(prefab, Parent);
             }
         }
-=======
-        var prefab = myLoadedAssetBundle.LoadAsset<GameObject>("pagesassetbundles");
-        Instantiate(prefab);
->>>>>>> 7096988c3f59170432ecc0d0437a7ad994ddc8d0
-=======
-        var prefab = myLoadedAssetBundle.LoadAsset<GameObject>("pagesassetbundles");
-        Instantiate(prefab);
->>>>>>> origin/main
-=======
-        var prefab = myLoadedAssetBundle.LoadAsset<GameObject>("pagesassetbundles");
-        Instantiate(prefab);
->>>>>>> origin/main
     }
 }
